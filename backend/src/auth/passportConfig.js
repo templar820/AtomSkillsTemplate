eafa1f448ebbefb09f424af2209ecdd4c5ccfbf1
templar820 <ttemplar820@gmail.com>
2021-06-05 12:27:32 +0300

    Регистрация - done

---
+++ backend/src/auth/passportConfig.js
@@ -0,0 +1,37 @@
+import passport from 'passport';
+import Local from 'passport-local';
+
+
+const LocalStrategy= Local.Strategy
+
+const userDB = {
+  id: 136345,
+  email: 'test@mail.ru',
+  password: '123',
+};
+
+passport.serializeUser(function(user, done) {
+  console.log('Сериализация: ', user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('Десериализация: ', id);
+  const user = userDB.id === id ? userDB : false;
+  done(null, user);
+});
+
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, function(
+    email,
+    password,
+    done
+  ) {
+    //Проверяем пользователя на наличие
+    if (email === userDB.email && password === userDB.password) {
+      return done(null, userDB);
+    } else {
+      return done(null, false);
+    }
+  })
+);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,51 @@
 import express from 'express'
 import router from "./router.js";
+import authRouter from "./auth/authRouter.js";
 import {errorHandler} from "./middleware/errorHandler.js";
 import fileUpload from 'express-fileupload';
 import sequelize from "./dbInit.js"
-// import passport from 'passport';
+import passport from 'passport';
 // import session from 'express-session'
-// import RedisStore from 'connect-redis'
+// import SessionFileStore from 'session-file-store'
+// const FileStore = SessionFileStore(session);
 
 
-const PORT = process.env.BACKEND_PORT;
+const PORT = process.env.BACKEND_PORT || 8080;
 
-const app = express()
-console.log(11111111111111111111111111111111);
+const app = express();
 
-
-try{
-  await sequelize.authenticate();
-  console.log("success")
-} catch (e) {
-  console.log(e);
-}
-
+//
+// try{
+//   await sequelize.authenticate();
+//   console.log("success")
+// } catch (e) {
+//   console.log(e);
+// }
 
 
+app.get('/', (req, res) => {
+  console.log(req.session);
+  res.send('HomePage');
+});
 
 app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
-// app.use(session({
-//   store: new RedisStore({
-//     url: config.redisStore.url
-//   }),
-//   secret: config.redisStore.secret,
-//   resave: false,
-//   saveUninitialized: false
-// }))
-// app.use(passport.initialize())
-// app.use(passport.session())
 
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(authRouter);
+
+const auth = (req, res, next) => {
+  console.log(req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    return res.redirect('/');
+  }
+};
+
+
 app.use(express.static('static'));
 app.use(fileUpload({}));
-app.use('/api', router);
+app.use('/api', auth, router);
 app.use(errorHandler);
