0b4dda6fae5080ac427ed8e70148b9dafd7bdd62
Nikolay Chikovani <nchikovany1@gmail.com>
2021-06-23 15:08:33 +0300

    i18n

--- frontend/src/components/System/Header.js
+++ frontend/src/components/System/Header.js
@@ -1,12 +1,30 @@
 import React, {useState} from 'react';
 import {inject, observer} from 'mobx-react';
+import { makeStyles } from '@material-ui/core/styles';
 import {Navbar} from "react-bootstrap";
 import AuthorizationCard from "../AuthorizationCard/AuthorizationCard";
 import {useTranslation} from "react-i18next";
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    width: '60px',
+    marginLeft: 'auto',
+    marginRight: '20px',
+  },
+}));
 
 const Header = () => {
   const [show, setShow] = useState(false);
-  const {t} = useTranslation();
+  const {t, i18n} = useTranslation();
+  const classes = useStyles();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
     <>
       <div className="d-flex flex-row align-items-center ml-4 mr-4 justify-content-between">
@@ -18,6 +36,16 @@ const Header = () => {
             <img src="https://cdn.eapteka.ru/local/templates/eapteka.v5/img/svg/sber-eapteka-logo.svg?_cvc=1621603449"
                  alt=""/></a>
         </Navbar>
+        <FormControl className={classes.formControl}>
+          <Select
+            defaultValue={i18n.language}
+            onChange={(e) => changeLanguage(e.target.value)}
+            label={t('header.language')}
+          >
+            <MenuItem value={'ru'}>RU</MenuItem>
+            <MenuItem value={'en'}>EN</MenuItem>
+          </Select>
+        </FormControl>
 
         <div className="d-flex flex-row align-items-center">
           <button
