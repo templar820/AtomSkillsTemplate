97adf8c108dcfbd3ae7f0a395dbdca6640264827
Nikolay Chikovani <nchikovany1@gmail.com>
2021-07-01 13:41:19 +0300

    product card

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './HomePage.scss';
 import {inject, observer} from 'mobx-react';
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import ContentLoader from "@/components/System/ContentLoader/ContentLoader";
+import {useTranslation} from "react-i18next";
 
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMap: false,
-    }
-  }
+const HomePage:React.FC = () => {
+  const {t} = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount() {
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 1000);
 
-  }
-
-  render() {
-    return (
-      <div className="home-page">
-        <h4 className="mb-3">Продукты</h4>
-        <div className="row">
-          {
-            [...Array(10).keys()].map((value) => (
-              <div key={value} className="home-page__card col-12 col-sm-6 col-lg-4 mb-3">
+  return (
+    <div className="home-page">
+      <div className="row">
+        <h4 className="mb-3 col-2">{t("homePage.products")}</h4>
+      </div>
+      <div className="row">
+        {
+          isLoading
+            ? [...Array(12).keys()].map((value) => (
+              <div key={value} className="product-card col-12 col-sm-6 col-lg-4 mb-3">
                 <ContentLoader/>
               </div>
             ))
-          }
-        </div>
+            : [...Array(12).keys()].map((value) => (
+              <div key={value} className="product-card col-12 col-sm-6 col-lg-4 mb-3">
+                <div className="product-card__wrapper card-body">
+                  <h5 className="card-title">Название продукта {value}</h5>
+                  <p className="card-text mb-1">Название вещества</p>
+                  <p className="card-text">Код вещества</p>
+                  <div className="d-flex justify-content-end">
+                    <Link to="/" className="btn btn-primary">Кнопка</Link>
+                  </div>
+                </div>
+              </div>
+            ))
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withRouter(inject("services")(observer(HomePage)));
