386f30072f00d9ab26837d62710055451faaaa2a
Nikolay Chikovani <nchikovany1@gmail.com>
2021-07-02 15:16:32 +0300

    head

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {inject, observer} from 'mobx-react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import {useTranslation} from "react-i18next";
 import './Header.scss';
@@ -12,19 +12,25 @@ import colors from '@styles/colors.modules.scss';
 import {IconButton} from "@material-ui/core";
 import {StoresNames} from "@/services/common/constDictionary";
 import {Link} from 'react-router-dom';
+import {ButtonGroup, DropdownButton, Dropdown} from "react-bootstrap";
+import UserStore from "@/stores/UserStore";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
-      borderColor: colors.white
+      border: "none",
     }
   },
   select: {
+    "& .MuiSelect-root": {
+      paddingBottom: 0,
+      paddingTop: 0,
+    },
     '&:before': {
-      borderColor: colors.white,
+      border: "none"
     },
     '&:after': {
-      borderColor: colors.white,
+      border: "none"
     },
     "& .MuiSvgIcon-root": {
       color: colors.white,
@@ -38,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const Header = (props: {services: any}) => {
+const Header = (props: { services: any }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const {t, i18n} = useTranslation();
   const classes = useStyles();
@@ -54,48 +60,41 @@ const Header = (props: {services: any}) => {
   return (
     <header className="header d-flex align-items-center fixed-top">
       <div className="container align-items-center">
-        <div className="row h-100">
-          <div className="col-4 d-flex align-items-center">
-            <Link to="/" className="d-flex align-items-center header__logo">
-              <div className="header__logo-box d-flex align-items-center justify-content-center">
-                <h2 className="header__logo-h2 m-0">HT</h2>
-              </div>
-              <h4 className="header__logo-h4 text-white ml-2 m-0">HackTemplate</h4>
-            </Link>
-          </div>
-          <div className="col-2 col-lg-1 offset-4 offset-lg-6 d-none d-sm-flex align-items-center justify-content-end">
-            <FormControl className={classes.formControl}>
-              <Select
-                defaultValue={i18n.language}
-                onChange={(e) => changeLanguage(e.target.value)}
-                label={t('header.language')}
-                className={classes.select}
-              >
-                <MenuItem value={'ru'}>RU</MenuItem>
-                <MenuItem value={'en'}>EN</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <div className="col-2 col-lg-1 d-none d-sm-flex align-items-center justify-content-end">
-            <div>
-              <IconButton aria-controls="profile-menu" aria-haspopup="true" onClick={(event)=>setAnchorEl(event.currentTarget)}>
-                <AccountCircleIcon className={classes.accountIcon} fontSize="large"/>
-              </IconButton>
-              <Menu
-                id="profile-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                transformOrigin={{ vertical: "top", horizontal: "center" }}
-                onClose={() => setAnchorEl(null)}
-              >
-                <MenuItem>Что-то там</MenuItem>
-                <MenuItem
-                  onClick={logout}
-                >{t('common.logout')}</MenuItem>
-              </Menu>
+        <div className="d-flex align-items-center h-100">
+          <Link to="/" className="d-flex align-items-center header__logo">
+            <div className="header__logo-box d-flex align-items-center justify-content-center">
+              <h2 className="header__logo-h2 m-0">HT</h2>
             </div>
+            <h4 className="header__logo-h4 text-white ml-2 m-0">HackTemplate</h4>
+          </Link>
+          <div className="ml-auto d-flex align-items-center h-100">
+            <DropdownButton
+              variant="outlined text-white d-none d-sm-block"
+              as={ButtonGroup}
+              title={t("header.translations")}
+            >
+              <Dropdown.Item
+                onClick={() => changeLanguage("ru")}
+              >RU</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => changeLanguage("en")}
+              >EN</Dropdown.Item>
+            </DropdownButton>
+            <DropdownButton
+              variant="outlined text-white pr-0"
+              as={ButtonGroup}
+              title={props[StoresNames.UserStore].user.email}
+            >
+              <Dropdown.Item
+                onClick={() => {
+              }}>Личный кабинет</Dropdown.Item>
+              <Dropdown.Divider/>
+              <Dropdown.Item
+                onClick={logout}
+              >
+                Выйти
+              </Dropdown.Item>
+            </DropdownButton>
           </div>
         </div>
       </div>
