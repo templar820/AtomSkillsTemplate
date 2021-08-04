61c3020697673227e2cfc1e192167e938c258c64
Кузнецов Иван Дмитриевич <41190483+templar820@users.noreply.github.com>
2021-08-04 17:07:08 +0300

    Мне кажется пора сессии оживить

--- backend/src/config/passportConfig.ts
+++ backend/src/config/passportConfig.ts
@@ -1,58 +1,59 @@
 import passport from 'passport';
 import Local from 'passport-local';
-import UserService from "../services/UserService";
-import passportJWT from "passport-jwt";
-const JWTStrategy =  passportJWT.Strategy;
+import passportJWT from 'passport-jwt';
+import UserService from '../services/UserService';
+
+const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
+const LocalStrategy = Local.Strategy;
 
-const LocalStrategy = Local.Strategy
-
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   console.log('Serrialize', user.email);
   done(null, user.id);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser((user, done) => {
   console.log('Deserialization', user);
   // const user = userDB.id === id ? userDB : false;
   done(null, user);
 });
 
-
-
 passport.use(
   new LocalStrategy({
     usernameField: 'email',
-  }, async function(
+  }, (async (
     email,
     password,
     done
-  ) {
-    //Проверяем пользователя на наличие
-    const user = await UserService.loginUser({email,password})
-    if(user){
-      const {email, id, password, role} = user;
-      return done(null, {email, id, password, role});
-    } else {
-      return done(null, false);
+  ) => {
+    // Проверяем пользователя на наличие
+    const user = await UserService.loginUser({ email, password });
+    if (user) {
+      const {
+        email, id, password, role
+      } = user;
+      return done(null, {
+        email, id, password, role
+      });
     }
-  })
+    return done(null, false);
+  }))
 );
 
 passport.use(
-    new JWTStrategy(
-        {
-          secretOrKey: process.env.SECRET_KEY || 'hacktemplate',
-          jwtFromRequest: ExtractJWT.fromHeader('token')
-        },
-        async (token, done) => {
-          console.log("IN JWT: ",token);
-          try {
-              return done(null, token);
-          } catch (error) {
-            done(error);
-          }
-        }
-    )
+  new JWTStrategy(
+    {
+      secretOrKey: process.env.SECRET_KEY || 'hacktemplate',
+      jwtFromRequest: ExtractJWT.fromHeader('token')
+    },
+    async (token, done) => {
+      console.log('IN JWT: ', token);
+      try {
+        return done(null, token);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
 );
--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,58 +1,47 @@
-import passport from "passport";
-import {asyncMiddleware} from "../middleware/asyncMiddleware";
-import UserController from "../controllers/UserController";
-import jwt from "jsonwebtoken";
-import {ServerError} from "../middleware/errorHandler";
-import BaseRouter from "./BaseRouter";
-import {auth} from "../middleware/authMiddleware";
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import { asyncMiddleware } from '../middleware/asyncMiddleware';
+import UserController from '../controllers/UserController';
+import { ServerError } from '../middleware/errorHandler';
+import BaseRouter from './BaseRouter';
+import { auth } from '../middleware/authMiddleware';
 
-
-
-class AuthRouter extends BaseRouter{
+class AuthRouter extends BaseRouter {
   constructor() {
     super();
-    this.createHandleWithBody('post', '/user/register', UserController.createUser)
-
+    this.createHandleWithBody('post', '/user/register', UserController.createUser);
 
     this.router.get('/user/logout', (req, res) => {
       req.logOut();
       res.sendFormat(null);
     });
 
-    this.router.post('/user/login', this.authenticate)
+    this.router.post('/user/login', this.authenticate);
 
     this.router.get('/user/userInfo', auth, asyncMiddleware(async (req, res) => {
       console.log(req.user.id);
       res.sendFormat(await UserController.getUserByToken(req.user.id));
-      }
-    ));
+    }));
   }
 
-
-  authenticate(req, res, next){
-      passport.authenticate('local', {session: false}, function(err, user) {
+  authenticate(req, res, next) {
+    passport.authenticate('local', { session: true }, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next(new ServerError(400, 'Incorrect login or password'));
+      }
+      req.logIn(user, { session: false }, (err) => {
         if (err) {
           return next(err);
         }
-        if (!user) {
-          return next(new ServerError(400, 'Incorrect login or password'));
-        }
-        req.logIn(user, {session: false}, function(err) {
-          if (err) {
-            return next(err);
-          }
-          const {email, id, role} = user
-          const token = jwt.sign({email, id, role}, process.env.SECRET_KEY || 'hacktemplate');
-          return res.sendFormat({token});
-        });
-      })(req, res, next);
-    };
+        const { email, id, role } = user;
+        const token = jwt.sign({ email, id, role }, process.env.SECRET_KEY || 'hacktemplate');
+        return res.sendFormat({ token });
+      });
+    })(req, res, next);
+  }
 }
-
-
-
-
-
-
 
 export default new AuthRouter().router;
