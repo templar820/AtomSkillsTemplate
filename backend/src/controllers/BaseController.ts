2e6d42e71713c5960fdb3142342efb1fc1081f77
templar820 <ttemplar820@gmail.com>
2021-08-05 10:31:22 +0300

    preparing to reactoring to socket connection

---
+++ backend/src/controllers/BaseController.ts
@@ -0,0 +1,8 @@
+import {Body, Controller, Get, Post, Route, Header, Tags, Security} from "tsoa";
+
+
+export default class BaseController extends Controller{
+  constructor(){
+   //сюда надо заинджектить
+  }
+}
