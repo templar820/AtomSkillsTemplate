1749c01365d1c918e2ff53397fcef1f7b86625d5
templar820 <ttemplar820@gmail.com>
2022-07-11 18:59:25 +0300

    data

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,6 +7,6 @@ console.log('POSTGRES_HOST', process.env.POSTGRES_HOST);
 
 export default new Sequelize(process.env.POSTGRES_DB || 'hacktemplate', process.env.POSTGRES_USER || 'user', process.env.POSTGRES_PASSWORD || 'pass', {
   // host: process.env.POSTGRES_HOST || 'localhost',
-  host: process.env.POSTGRES_HOST || '172.17.01',
+  host: process.env.POSTGRES_HOST || '172.17.0.1',
   dialect: 'postgres'
 });
